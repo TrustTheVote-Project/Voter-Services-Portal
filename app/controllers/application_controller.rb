6023ff1e4d05e540964a234321b502b187912246
Aleksey Gureiev <spyromus@noizeramp.com>
2012-01-21 22:24:02 +0200

    Added filter to require active registration.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,12 @@ class ApplicationController < ActionController::Base
     @current_registration ||= RegistrationRepository.get_registration(session[:session_id])
   end
 
+  protected
+
+  # Filter that makes sure there's a registration object
+  # available, otherwise redirects to the front page.
+  def requires_registration
+    redirect_to :root unless current_registration
+  end
+
 end
