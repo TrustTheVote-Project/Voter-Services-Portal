a49b0635ab2cc201086e8254261e405e9242d387
Aleksey Gureiev <spyromus@noizeramp.com>
2012-01-24 15:18:24 +0200

    Code review.

--- app/models/absentee_status_request.rb
+++ app/models/absentee_status_request.rb
@@ -1,7 +1,6 @@
 class AbsenteeStatusRequest
 
   include ActiveModel::Validations
-  #include ActiveModel::MassAssignmentSecurity
 
   attr_accessor :full_name, :ssn, :vote_in, :location_type, :location_name, :vote_on
 
--- app/models/registration.rb
+++ app/models/registration.rb
@@ -2,7 +2,7 @@ class Registration < ActiveRecord::Base
 
   scope :stale, lambda { where([ "created_at < ?", 1.day.ago ]) }
 
-  attr_accessible :voter_id
+  attr_accessible :first_name, :middle_name, :last_name, :suffix_name_text, :ssn
 
   def full_name
     [ first_name, middle_name, last_name, suffix_name_text ].delete_if(&:blank?).join(' ')
