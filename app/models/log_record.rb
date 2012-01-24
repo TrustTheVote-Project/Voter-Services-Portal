f67ff2887b64ce489fb104bfce233c0a2eae2778
Aleksey Gureiev <spyromus@noizeramp.com>
2012-01-24 15:14:18 +0200

    Security review.

--- app/models/log_record.rb
+++ app/models/log_record.rb
@@ -2,6 +2,8 @@ class LogRecord < ActiveRecord::Base
 
   serialize :details
 
+  attr_accessible :subject, :action, :details
+
   # Makes a log record
   def self.log(subject, action, details = nil)
     LogRecord.create(subject: subject, action: action, details: details)
--- app/models/registration.rb
+++ app/models/registration.rb
@@ -2,6 +2,8 @@ class Registration < ActiveRecord::Base
 
   scope :stale, lambda { where([ "created_at < ?", 1.day.ago ]) }
 
+  attr_accessible :voter_id
+
   def full_name
     [ first_name, middle_name, last_name, suffix_name_text ].delete_if(&:blank?).join(' ')
   end
