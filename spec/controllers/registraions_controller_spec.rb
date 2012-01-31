005e24ac0a1d63a6babda3e68a64e8f1f2e5001c
Aleksey Gureiev <spyromus@noizeramp.com>
2012-01-31 21:02:28 +0200

    Removed outdated spec.

--- spec/controllers/registraions_controller_spec.rb
+++ spec/controllers/registraions_controller_spec.rb
@@ -2,10 +2,4 @@ require 'spec_helper'
 
 describe RegistrationsController do
 
-  describe 'new' do
-    before { LogRecord.should_receive(:log).with('registration', 'started') }
-    before { get :new }
-    it     { should render_template :new }
-  end
-
 end
