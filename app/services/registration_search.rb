52772a2484ef3cc76001924d9f52892b7a36a136
Aleksey Gureiev <spyromus@noizeramp.com>
2012-01-31 17:26:20 +0200

    Not instantiating Registration objects in seeds.

--- app/services/registration_search.rb
+++ app/services/registration_search.rb
@@ -2,7 +2,7 @@
 class RegistrationSearch
 
   SEED_DATA = {
-    '1' => Registration.new(
+    '1' => {
       first_name:         'Wanda',
       middle_name:        'Hunt',
       last_name:          'Phepts',
@@ -12,9 +12,9 @@ class RegistrationSearch
       dob:                Date.parse('1927-11-06 00:00:00.000'),
       party_affiliation:  'Democrat',
       voting_address:     '518 Vance St, Bristol, VA 242012445',
-      mailing_address:    '518 Vance St, Bristol, VA 242012445'),
+      mailing_address:    '518 Vance St, Bristol, VA 242012445' },
 
-    '2' => Registration.new(
+    '2' => {
       first_name:         'Jack',
       last_name:          'Morgan',
       phone:              '555-555-5555',
@@ -23,9 +23,9 @@ class RegistrationSearch
       party_affiliation:  'Republican',
       voting_address:     '518 Oak St, Bristol, VA 242012445',
       mailing_address:    '518 Oak St, Bristol, VA 242012445',
-      absentee:           true),
+      absentee:           true },
 
-    '3' => Registration.new(
+    '3' => {
       first_name:         'Michael',
       last_name:          'Phepts',
       phone:              '540-555-1200',
@@ -35,12 +35,13 @@ class RegistrationSearch
       voting_address:     '518 Vance St, Bristol, VA 242012445',
       mailing_address:    '518 Vance St, Bristol, VA 242012445',
       absentee:           true,
-      uocava:             true),
+      uocava:             true }
   }
 
   def self.perform(search_query)
     # TODO replace this hack we use for tests with real search
-    SEED_DATA[search_query.voter_id]
+    data = SEED_DATA[search_query.voter_id]
+    data ? Registration.new(data) : nil
   end
 
 end
