9ebd5e0000311fea8e19f9e4d6835e6742e480aa
Aleksey Gureiev <spyromus@noizeramp.com>
2012-02-17 21:39:48 +0200

    filled city-county list

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module VaVote
     # -- all .rb files in that directory are automatically loaded.
 
     # Custom directories with classes and modules you want to be autoloadable.
-    config.autoload_paths += %W(#{Rails.root}/app/services #{Rails.root}/app/presenters)
+    config.autoload_paths += %W(#{Rails.root}/app/services #{Rails.root}/app/presenters #{Rails.root}/lib)
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
