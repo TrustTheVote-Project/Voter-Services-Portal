5e10015a7df8a8d06f88dc07af3f5b034015c2ff
Aleksey Gureiev <spyromus@noizeramp.com>
2012-04-30 18:13:14 +0300

    addresses section

--- app/presenters/registration_request_for_pdf.rb
+++ app/presenters/registration_request_for_pdf.rb
@@ -41,7 +41,7 @@ class RegistrationRequestForPdf
   end
 
   def ssn
-    n = @req.ssn
+    n = @req.ssn.to_s.gsub(/[^0-9]/, '')
     return '' if n.blank?
 
     "#{n[0, 3]}-#{n[3, 2]}-#{n[5, 4]}"
@@ -66,23 +66,59 @@ class RegistrationRequestForPdf
   # Addresses
 
   def registration_address
-    "RR 2 Box 23, San Francisco, CA 94110"
+    @vvr ||= begin
+      if @req.vvr_is_rural == '0'
+        if @req.vvr_county_or_city.downcase.include?('county')
+          county = @req.vvr_county_or_city
+          city = @req.vvr_town_or_city
+        else
+          county = ''
+          city = @req.vvr_county_or_city
+        end
+
+        zip = [ @req.vvr_zip5, @req.vvr_zip4 ].reject(&:blank?).join('-')
+        [ [ @req.vvr_street_number, @req.vvr_street_name, @req.vvr_street_suffix ].reject(&:blank?).join(' '),
+          county,
+          city,
+          [ 'VA', zip ].join(' ') ].reject(&:blank?).join(', ')
+      else
+        @req.vvr_rural
+      end
+    end
   end
 
   def mailing_address
-    "P.O. Box 876, Fredericksberg, VA 22412"
+    if @req.ma_other == '0'
+      registration_address
+    else
+      us_address(:ma)
+    end
   end
 
   def previous_registration?
-    true
+    @req.has_existing_reg == 'true'
   end
 
   def previous_registration_address
-    "456 Junipero Serra St., San Francisco, CA 94110"
+    us_address(:er)
   end
 
   def address_confidentiality?
     true
   end
 
+  private
+
+  def us_address(prefix)
+    if @req.send("#{prefix}_is_rural")
+      @req.send("#{prefix}_rural")
+    else
+      [ @req.send("#{prefix}_address"),
+        @req.send("#{prefix}_address_2"),
+        @req.send("#{prefix}_city"),
+        @req.send("#{prefix}_state"),
+        [ @req.send("#{prefix}_zip5"), @req.send("#{prefix}_zip4") ].reject(&:blank?).join('-')
+      ].reject(&:blank?).join(', ')
+    end
+  end
 end
--- app/views/registration_requests/_addresses.pdf.prawn
+++ app/views/registration_requests/_addresses.pdf.prawn
@@ -7,24 +7,28 @@ pdf_labeled_block pdf, "Addresses" do
 
     if rr.previous_registration?
       pdf.move_down 15
-      pdf_fields pdf, [
-        { columns: 1, value: rr.previous_registration_address, label: 'previous registration address' }
-      ]
+      pdf_full_width_block pdf do |heights|
+        heights << pdf_column_block(pdf, 2, 1, 0) do
+          pdf_fields pdf, [
+            { columns: 1, value: rr.previous_registration_address, label: 'previous registration address' }
+          ]
+        end
+
+        heights << pdf_column_block(pdf, 2, 1, 1) do
+          pdf.indent 10 do
+            pdf_checkbox pdf, "Cancel my existing registration."
+          end
+        end
+      end
     end
 
-    if rr.previous_registration? || rr.address_confidentiality?
+    if rr.address_confidentiality?
       pdf.move_down 10
     end
 
     pdf_full_width_block pdf do |heights|
-      if rr.previous_registration?
-        heights << pdf_column_block(pdf, 2, 1, 0) do
-          pdf_checkbox pdf, "Cancel my existing registration."
-        end
-      end
-
       if rr.address_confidentiality?
-        heights << pdf_column_block(pdf, 2, 1, 1) do
+        heights << pdf_column_block(pdf, 2, 1, 0) do
           pdf_checkbox pdf, "I quality for address confidentiality."
         end
       end
