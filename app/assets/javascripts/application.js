652c0de17063f80abe4e5c2fe23f982a533b48dc
Aleksey Gureiev <spyromus@noizeramp.com>
2012-05-11 20:00:52 +0300

    registration workflow visual rework

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
+//= require public/html5shiv
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-tooltip
