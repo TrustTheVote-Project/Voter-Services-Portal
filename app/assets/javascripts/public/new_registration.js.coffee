6e8ea5d4136fc8aa6362ddd9ee35c32f070e8660
Aleksey Gureiev <spyromus@noizeramp.com>
2012-05-13 21:50:23 +0300

    showing / hiding blocks

---
+++ app/assets/javascripts/public/new_registration.js.coffee
@@ -0,0 +1,36 @@
+window.stepClass = (current, idx, def) ->
+  def   = def || 'span2'
+  match = if $.isArray(idx) then idx.indexOf(current) > -1 else idx == current
+  max   = if $.isArray(idx) then idx[idx.length - 1] else idx
+  (if match then 'current ' else if current > max then 'done ' else '') + def
+
+class NewRegistration
+  constructor: (initPage = 0) ->
+    @pages = [ 'eligibility', 'identity', 'address', 'options', 'confirm', 'oath', 'download', 'congratulations' ]
+
+    # Eligibility
+    @rightsWereRevoked      = ko.observable()
+    @rightsRevokationReason = ko.observable()
+    @rightsWereRestored     = ko.observable()
+
+    # Addresses
+    @vvrIsRural             = ko.observable(false)
+    @maIsSame               = ko.observable('yes')
+    @hasExistingReg         = ko.observable('no')
+    @erIsRural              = ko.observable(false)
+
+    # Options
+    @isConfidentialAddress  = ko.observable(false)
+    @requestingAbsentee     = ko.observable(false)
+    @rabElection            = ko.observable()
+    @abSendTo               = ko.observable()
+
+    # Navigation
+    @currentPageIdx         = ko.observable(initPage)
+    @page                   = ko.computed(=> @pages[@currentPageIdx()])
+
+  # Navigation
+  prevPage: => @currentPageIdx(@currentPageIdx() - 1)
+  nextPage: => @currentPageIdx((@currentPageIdx() + 1) % @pages.length)
+
+ko.applyBindings(new NewRegistration(3))
--- app/models/registration_request.rb
+++ app/models/registration_request.rb
@@ -21,17 +21,27 @@ class RegistrationRequest < ActiveRecord::Base
   serialized_attr :phone, :email
 
   # Contact info
-  serialized_attr :vvr_county_or_city, :vvr_street_number, :vvr_street_name, :vvr_street_type, :vvr_street_suffix
-  serialized_attr :vvr_town_or_city, :vvr_zip5, :vvr_zip4
+  serialized_attr :vvr_county_or_city, :vvr_street_number, :vvr_street_name, :vvr_street_type, :vvr_street_suffix, :vvr_apt
+  serialized_attr :vvr_town_or_city, :vvr_state, :vvr_zip5, :vvr_zip4
   serialized_attr :vvr_is_rural, :vvr_rural
   serialized_attr :vvr_uocava_residence_available, :vvr_uocava_residence_unavailable, :vvr_uocava_residence_unavailable_since
   serialized_attr :raa_address, :raa_address_2, :raa_city, :raa_city_2, :raa_state, :raa_postal_code, :raa_country
   serialized_attr :mau_address, :mau_address_2, :mau_city, :mau_city_2, :mau_state, :mau_postal_code, :mau_country, :mau_type
-  serialized_attr :ma_address,  :ma_address_2,  :ma_city, :ma_state, :ma_zip5, :ma_zip4, :ma_is_rural, :ma_rural, :ma_other
+  serialized_attr :ma_address,  :ma_address_2,  :ma_city, :ma_state, :ma_zip5, :ma_zip4, :ma_is_same
   serialized_attr :apo_address, :apo_1, :apo_2, :apo_zip5
-  serialized_attr :is_confidential_address, :ca_type
   serialized_attr :has_existing_reg, :er_cancel
-  serialized_attr :er_address,  :er_address_2, :er_city, :er_state, :er_zip5, :er_zip4, :er_is_rural, :er_rural, :er_other
+  serialized_attr :er_street_number, :er_street_name, :er_street_type, :er_apt,
+                  :er_city, :er_state, :er_zip5, :er_zip4, :er_is_rural, :er_rural
+
+  # Options
+  serialized_attr :is_confidential_address, :ca_type
+  serialized_attr :requesting_absentee, :rab_election,
+                  :rab_election_name, :rab_election_date
+  serialized_attr :ab_reason, :ab_school_name, :ab_street_number,
+                  :ab_street_name, :ab_street_type,
+                  :ab_apt, :ab_city, :ab_state, :ab_zip5, :ab_zip4, :ab_country,
+                  :ab_send_to, :aba_address, :aba_address_2,
+                  :aba_city, :aba_state, :aba_zip5, :aba_zip4, :aba_country
   serialized_attr :be_official
 
   # Complete Registration
