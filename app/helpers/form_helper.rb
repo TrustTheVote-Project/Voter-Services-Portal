fcdcc5c7e10f3f39469058ba321b9c80812ecca5
Aleksey Gureiev <spyromus@noizeramp.com>
2012-06-05 13:17:18 +0300

    fixed incorrect naming of the data fields

--- app/helpers/form_helper.rb
+++ app/helpers/form_helper.rb
@@ -12,14 +12,14 @@ module FormHelper
     start_year = options[:start_year] || Date.today.year - 150
     end_year   = options[:end_year] || Date.today.year - 17
 
-    months = options_for_select([ nil ] + 12.times.map { |i| [ dts.send(:month_name, i + 1), i + 1 ] })
-    days   = options_for_select([ nil ] + (1 .. 31).to_a)
-    years  = options_for_select([ nil ] + (start_year .. end_year).to_a.reverse)
-    field  = field.to_s.camelcase(:lower)
+    months  = options_for_select([ nil ] + 12.times.map { |i| [ dts.send(:month_name, i + 1), i + 1 ] })
+    days    = options_for_select([ nil ] + (1 .. 31).to_a)
+    years   = options_for_select([ nil ] + (start_year .. end_year).to_a.reverse)
+    jsfield = field.to_s.camelcase(:lower)
 
-    [ select_tag("registration_request[#{field}(2i)]", months, 'data-bind' => "value: #{field}Month"),
-      select_tag("registration_request[#{field}(3i)]", days, 'data-bind' => "value: #{field}Day"),
-      select_tag("registration_request[#{field}(1i)]", years, 'data-bind' => "value: #{field}Year")
+    [ select_tag("registration_request[#{field}(2i)]", months, 'data-bind' => "value: #{jsfield}Month"),
+      select_tag("registration_request[#{field}(3i)]", days, 'data-bind' => "value: #{jsfield}Day"),
+      select_tag("registration_request[#{field}(1i)]", years, 'data-bind' => "value: #{jsfield}Year")
     ].join(' ').html_safe
   end
 
