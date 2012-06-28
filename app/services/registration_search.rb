bcb382dc9cd4ac9447bdbfd1bc42c076ae95d413
Aleksey Gureiev <spyromus@noizeramp.com>
2012-06-28 12:15:25 +0300

    added domestic absentee sample record

--- app/services/registration_search.rb
+++ app/services/registration_search.rb
@@ -65,6 +65,45 @@ class RegistrationSearch
 
       has_existing_reg:   'no',
       absentee_until:     2.months.from_now
+    },
+
+    '4444333322221111' => {
+      residence:          'in',
+      requesting_absentee: true,
+      first_name:         'Domestic',
+      last_name:          'Absentee',
+      gender:             'Male',
+      dob:                40.years.ago,
+      ssn4:               '1234',
+
+      vvr_street_number:  '5',
+      vvr_street_name:    'First',
+      vvr_street_type:    'ST',
+      vvr_county_or_city: 'BRISTOL CITY',
+      vvr_state:          'VA',
+      vvr_zip5:           '12345',
+      vvr_zip4:           '',
+
+      ma_is_same:         'yes',
+
+      is_confidential_address: '1',
+      ca_type:            'if',
+
+      rab_election:       '1',
+      ab_reason:          'Reason 2',
+      ab_school_name:     'St Joseph',
+      ab_street_number:   '51',
+      ab_street_name:     'Church',
+      ab_street_type:     'ST',
+      ab_apt:             '1',
+      ab_city:            'Bristol',
+      ab_state:           'VA',
+      ab_zip5:            '12345',
+      ab_zip4:            '6789',
+      ab_country:         'USA',
+
+      has_existing_reg:   'no',
+      be_official:        '1'
     }
   }
 
