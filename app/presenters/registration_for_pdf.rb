6b7ce2e35d19fb3e19cda74b3d9b5a5a0ed59ed3
Aleksey Gureiev <spyromus@noizeramp.com>
2012-09-03 08:24:06 +0300

    [pdf] prepernding "P.O. Box"

--- app/presenters/registration_for_pdf.rb
+++ app/presenters/registration_for_pdf.rb
@@ -104,7 +104,8 @@ class RegistrationForPdf < RegistrationDetailsPresenter
 
   def acp_pobox(d = :data)
     d = @reg.send(d)
-    [ d[:ca_po_box], d[:ca_city], [ d[:ca_zip5], d[:ca_zip4] ].reject(&:blank?).join('-') ].join(', ')
+    po = d[:ca_po_box].blank? ? '' : "P.O. Box #{d[:ca_po_box]}"
+    [ po, d[:ca_city], [ d[:ca_zip5], d[:ca_zip4] ].reject(&:blank?).join('-') ].join(', ')
   end
 
   # Military / overseas
