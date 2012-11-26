af1883785ff8db6998c1aacb1b905ec8cb7e71e0
Aleksey Gureiev <spyromus@noizeramp.com>
2012-11-26 12:13:45 +0200

    initial json lookup

--- app/controllers/api/registrations_controller.rb
+++ app/controllers/api/registrations_controller.rb
@@ -1,3 +1,40 @@
 class Api::RegistrationsController < ActionController::Metal
+
+  class InvalidSearchQuery < StandardError; end
+
   include ActionController::Rendering
+
+  # searches for registrations
+  def show
+    @query = build_search_query
+    render_json success: true, record: ''
+  rescue InvalidSearchQuery
+    render_error 'Invalid search parameters'
+  end
+
+  private
+
+  # builds search query from request parameters or
+  # raises the #InvalidSearchQuery# if parameters are incomplete.
+  def build_search_query
+    p     = params.except('action', 'controller')
+    query = SearchQuery.new(p)
+
+    raise InvalidSearchQuery unless query.valid?
+
+    query
+  end
+
+  # renders the error
+  def render_error(msg)
+    @json_response = { success: false, error: msg }
+    render text: @json_response.to_json, status: 500
+  end
+
+  # renders json
+  def render_json(hash)
+    @json_response = hash
+    render text: @json_response.to_json
+  end
+
 end
--- spec/controllers/api/registrations_controller_spec.rb
+++ spec/controllers/api/registrations_controller_spec.rb
@@ -1,4 +1,24 @@
 require 'spec_helper'
 
-describe Api::RegistrationsController do
+describe Api::RegistrationsController, :focus do
+
+  it 'should return an error if the query parameters are invalid' do
+    get :show
+    response.should be_error
+    assigns(:json_response).should == { 'success' => false, 'error' => 'Invalid search parameters' }
+  end
+
+  it 'should return existing record by voter ID' do
+    get :show, voter_id: 600000000, locality: 'NORFOLK CITY'
+    response.should be_success
+
+    r = assigns(:json_response)
+    r['success'].should be_true
+    r['record'].should be
+  end
+
+  it 'should return existing record by SSN'
+
+  it 'should return 404 when not found'
+
 end
