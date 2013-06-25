f53e7d938e6e53f8972788e0b14bfd6d82de5e0b
Aleksey Gureiev <spyromus@noizeramp.com>
2013-06-25 14:09:34 +0300

    updated specs

--- spec/features/processing_update_eml310_feature_spec.rb
+++ spec/features/processing_update_eml310_feature_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-feature 'Processing update EML310 submission', :js, :focus do
+feature 'Processing update EML310 submission', :js do
 
   before do
     Office.create!(locality: "NORFOLK CITY", address: "Some address")
