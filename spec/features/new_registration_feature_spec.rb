81c7cbbdcc6f3b9a0d320247b72aad9d49901957
Aleksey Gureiev <spyromus@noizeramp.com>
2013-10-30 08:34:28 +0200

    fixes

--- spec/features/new_registration_feature_spec.rb
+++ spec/features/new_registration_feature_spec.rb
@@ -3,6 +3,8 @@ require 'spec_helper'
 feature 'New registration', :js do
 
   before { seed_offices }
+  before { @old_enable_dmvid_lookup = AppConfig['enable_dmvid_lookup'] }
+  after  { AppConfig['enable_dmvid_lookup'] = @old_enable_dmvid_lookup }
 
   scenario 'DMV match should populate the address section' do
     LookupService.stub(registration: { registered: false, dmv_match: true, address: { address_1: "123 WannaVote DR", zip5: "12345", county_or_city: "ALEXANDRIA CITY" }})
@@ -18,8 +20,10 @@ feature 'New registration', :js do
   end
 
   scenario 'w/ special page instead of d/l for unregistered DMV matches' do
+    AppConfig['enable_dmvid_lookup'] = true
     LookupService.stub(registration: { registered: false, dmv_match: true })
     SubmitEml310.stub(submit_new: true)
+
     visit '/register/residential'
 
     fill_eligibility_page
@@ -34,6 +38,7 @@ feature 'New registration', :js do
   end
 
   scenario 'w/ d/l screen for normal records' do
+    AppConfig['enable_dmvid_lookup'] = true
     LookupService.stub(registration: { registered: false, dmv_match: false })
     SubmitEml310.stub(submit_new: false)
     visit '/register/residential'
