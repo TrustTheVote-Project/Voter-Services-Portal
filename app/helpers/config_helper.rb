21ccac36c7508dcd1157fe98a922b25664dd9053
Aleksey Gureiev <spyromus@noizeramp.com>
2014-09-15 11:21:28 +0400

    added customizations for CSS

--- app/helpers/config_helper.rb
+++ app/helpers/config_helper.rb
@@ -4,4 +4,10 @@ module ConfigHelper
     @enabled_uocava_reg ||= AppConfig['enable_uocava_new_registration']
   end
 
+  def custom_css_link_tag(name)
+    if v = AppConfig['customization'].try(:[], name)
+      stylesheet_link_tag v, :media => 'all'
+    end
+  end
+
 end
